b0fe4a9a6b2e439575cd5ca5f223feec05593519
npgy <me@np.gy>
2023-08-13 00:03:39 -0400

    attempt it again

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Host $1" >> /root/.ssh/config
 echo "ProxyCommand cloudflared access ssh --hostname %h" >> /root/.ssh/config
-echo $4 >> /root/.ssh/key.pem
+echo "$4" > /root/.ssh/key.pem
 chmod 600 /root/.ssh/key.pem
 
 ssh-keyscan $1 >> /root/.ssh/known_hosts
