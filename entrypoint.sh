366b78faa7fa39ee8d45cbb58737c5cb6a3ff125
npgy <me@np.gy>
2023-08-12 23:32:35 -0400

    try this one instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,6 @@ touch /root/.ssh/known_hosts
 ssh-keyscan $1 >> /root/.ssh/known_hosts
 
 # ssh -o StrictHostKeyChecking=no $3@$1 uptime
-eval $(ssh-agent)
-ssh-add - <<< "$4"
+ssh-add /root/.ssh/key.pem
 # ssh -i /root/.ssh/key.pem $3@$1 -p $2 "$5"
 ssh $3@$1 -p $2 "$5"
