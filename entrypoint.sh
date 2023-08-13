373a99ab5a86d08948baebf1a6b0803a8faa222d
npgy <me@np.gy>
2023-08-12 23:16:14 -0400

    try some hacky thing

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ echo "Host $1" >> /root/.ssh/config
 echo "ProxyCommand cloudflared access ssh --hostname %h" >> /root/.ssh/config
 echo $4 >> /root/.ssh/key.pem
 
+ssh -o StrictHostKeyChecking=no $3@$1 uptime
 ssh -i /root/.ssh/key.pem $3@$1 -p $2 "$5"
