7dcb3f006747488f5d45966b8ca83d1592a016b4
npgy <me@np.gy>
2023-08-12 23:54:06 -0400

    hopefully last time

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,8 @@ echo "ProxyCommand cloudflared access ssh --hostname %h" >> /root/.ssh/config
 echo $4 >> /root/.ssh/key.pem
 chmod 600 /root/.ssh/key.pem
 
-touch /root/.ssh/known_hosts
 ssh-keyscan $1 >> /root/.ssh/known_hosts
 
-# ssh -o StrictHostKeyChecking=no $3@$1 uptime
-ssh-add /root/.ssh/key.pem
-# ssh -i /root/.ssh/key.pem $3@$1 -p $2 "$5"
-ssh $3@$1 -p $2 "$5"
+ssh -o StrictHostKeyChecking=no $3@$1
+
+ssh -i /root/.ssh/key.pem $3@$1 -p $2 "$5"
