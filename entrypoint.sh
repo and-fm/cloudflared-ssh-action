687e4a2dd1293360c8bf178f35ce6c82acc3ba04
npgy <me@np.gy>
2023-08-12 23:18:02 -0400

    fix private key perms

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 echo "Host $1" >> /root/.ssh/config
 echo "ProxyCommand cloudflared access ssh --hostname %h" >> /root/.ssh/config
 echo $4 >> /root/.ssh/key.pem
+chmod 600 /root/.ssh/key.pem
 
 ssh -o StrictHostKeyChecking=no $3@$1 uptime
 ssh -i /root/.ssh/key.pem $3@$1 -p $2 "$5"
