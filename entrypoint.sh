89c807615efe3fedfda4c155a7c27dd86f246bfc
nathanjnorris <13533617+nathanjnorris@users.noreply.github.com>
2024-03-17 20:40:40 -0700

    Add service token to cloudflared command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 echo "Host $1" >> /root/.ssh/config
-echo "ProxyCommand cloudflared access ssh --hostname %h" >> /root/.ssh/config
+echo "ProxyCommand cloudflared access ssh --hostname %h --id $6 --secret $7" >> /root/.ssh/config
 echo "$4" > /root/.ssh/key.pem
 chmod 600 /root/.ssh/key.pem
 
