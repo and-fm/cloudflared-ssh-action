431c51bcfc59a7377782c9d2a072136095309864
npgy <me@np.gy>
2024-05-02 18:26:54 -0400

    ah i think i found it

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,16 @@ echo "Host $1" >> /root/.ssh/config
 
 if [ -z $7 ] && [ -z $8 ]
 then
-    echo "ProxyCommand cloudflared access ssh --hostname %h >> /root/.ssh/config
+    echo "ProxyCommand cloudflared access ssh --hostname %h" >> /root/.ssh/config
 else
     echo "ProxyCommand cloudflared access ssh --hostname %h --id $7 --secret $8" >> /root/.ssh/config
 fi
 
-echo "$5" > /root/.ssh/key.pem
-chmod 600 /root/.ssh/key.pem
+echo "$5" > /root/.ssh/$4
+chmod 600 /root/.ssh/$4
 
 ssh-keyscan $1 >> /root/.ssh/known_hosts
 
 ssh -T -q -o StrictHostKeyChecking=no $3@$1
 
-ssh -i /root/.ssh/key.pem $3@$1 -p $2 "$6"
+ssh -i /root/.ssh/$4 $3@$1 -p $2 "$6"
